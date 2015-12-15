fb16a2d419cd7f30274323509fa505336847038d
zhanghuangang <huangangzhang@gmail.com>
2015-12-15 13:35:36 +0800

    update theme

---
+++ source/js/app.js
@@ -0,0 +1,52 @@
+/**
+ * Created by zhg on 15/12/15.
+ */
+
+$(function () {
+    var stack = [];
+    var $container = $('.js_container');
+    $container.on('click', '.js_cell[data-id]', function () {
+        var id = $(this).data('id');
+        go(id);
+    });
+
+    function go(id){
+        var $tpl = $($('#tpl_' + id).html()).addClass('slideIn').addClass(id);
+        $container.append($tpl);
+        stack.push($tpl);
+        location.hash = '#' + id;
+        $($tpl).on('webkitAnimationEnd', function (){
+            $(this).removeClass('slideIn');
+        }).on('animationend', function (){
+            $(this).removeClass('slideIn');
+        });
+        // tooltips
+        if (id == 'cell') {
+            $('.js_tooltips').show();
+            setTimeout(function (){
+                $('.js_tooltips').hide();
+            }, 3000);
+        }
+    }
+    function hideActionSheet(weuiActionsheet, mask) {
+        weuiActionsheet.removeClass('weui_actionsheet_toggle');
+        mask.removeClass('weui_fade_toggle');
+        weuiActionsheet.on('transitionend', function () {
+            mask.hide();
+        }).on('webkitTransitionEnd', function () {
+            mask.hide();
+        })
+    }
+    $container.on('click','#showActionSheet', function () {
+        var mask = $('#mask');
+        var weuiActionsheet = $('#weui_actionsheet');
+        weuiActionsheet.addClass('weui_actionsheet_toggle');
+        mask.show().addClass('weui_fade_toggle').click(function () {
+            hideActionSheet(weuiActionsheet, mask);
+        });
+        $('#actionsheet_cancel').click(function () {
+            hideActionSheet(weuiActionsheet, mask);
+        });
+        weuiActionsheet.unbind('transitionend').unbind('webkitTransitionEnd');
+    });
+});
